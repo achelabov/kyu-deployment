0c7a918ec6d0bd9f84a25b5258e43a44621bd2eb
achelabov <ventux228@mail.ru>
2025-01-16 00:14:36 +0300

    change script

--- change_script_old.sh
+++ change_script_old.sh
@@ -1,8 +1,9 @@
 echo 'changing app...'
 
 # change app
-docker exec -it kyu-deployment-swetrix-1 /bin/sh -c 'rm -rf /app'   
-docker cp app kyu-deployment-swetrix-1:/app
+docker exec -it kyu-deployment-swetrix-1 /bin/sh -c 'rm -rf app && rm -rf public'   
+docker cp app/app kyu-deployment-swetrix-1:/app/app
+docker cp app/public kyu-deployment-swetrix-1:/app/public
 
 # rebuild and run
 docker exec -it kyu-deployment-swetrix-1 /bin/sh -c 'npm run build'
